eff85307cb275109ac74a83c0418aac5869f7461
Jakub Oleksiak <s18728@pjwstk.edu.pl>
2020-03-23 02:31:31 +0100

    Ćwiczenia 4 - kontynuacja dokończonych ćwiczeń 3, zrobione do zadania 4.2 włącznie.

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using cw3.DAL;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace cw3.Controllers
 {
@@ -32,51 +34,77 @@ namespace cw3.Controllers
         //     return NotFound("Nie znaleziono studenta!");
         // }
 
-        private readonly IDbService _dbService;
+        // private readonly IDbService _dbService;
+        //
+        // public StudentsController(IDbService dbService)
+        // {
+        //     _dbService = dbService;
+        // }
 
-        public StudentsController(IDbService dbService)
-        {
-            _dbService = dbService;
-        }
+        // private static List<Student> students = new List<Student>();
 
-        private static List<Student> students = new List<Student>();
         [HttpGet]
-        public IActionResult GetStudent(string orderBy)
+        public IActionResult GetStudent()
         {
-            return Ok(_dbService.GetStudents());
-        }
-
-        [HttpPost]
-        public IActionResult CreateStudent(Student student)
-        {
-            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
-            students.Add(student);
-            return Ok(student);
-        }
-
-        [HttpPut]
-        public IActionResult UpdateStudent([FromQuery]int id, [FromBody] Student student)
-        {
-            if (id>=students.Count)
+            List<Student> students = new List<Student>();
+            using (var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18728;Integrated Security=True"))
+            using (var com = new SqlCommand())
             {
-                return NotFound();
+                com.Connection = con;
+                com.CommandText =
+                    "SELECT FirstName, LastName, BirthDate, Semester, Name FROM Student " +
+                    "INNER JOIN Enrollment ON Student.IdEnrollment = Enrollment.IdEnrollment " +
+                    "INNER JOIN Studies ON Studies.IdStudy = Enrollment.IdStudy";
+
+                con.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+                    st.NameOfStudies = dr["Name"].ToString();
+                    students.Add(st);
+                }
+
             }
 
-            students.RemoveAt(id);
-            students.Insert(id, student);
-            return Ok("Aktualizacja ukonczona");
+            return Ok(students);
         }
 
-        [HttpDelete]
-        public IActionResult DeleteStudent([FromQuery] int id)
-        {
-            if (id >= students.Count)
-            {
-                return NotFound();
-            }
+        // [HttpPost]
+        // public IActionResult CreateStudent(Student student)
+        // {
+        //     student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+        //     students.Add(student);
+        //     return Ok(student);
+        // }
 
-            students.RemoveAt(id);
-            return Ok("Usunieto studenta o id " + id);
-        }
+        // [HttpPut]
+        // public IActionResult UpdateStudent([FromQuery]int id, [FromBody] Student student)
+        // {
+        //     if (id>=students.Count)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     students.RemoveAt(id);
+        //     students.Insert(id, student);
+        //     return Ok("Aktualizacja ukonczona");
+        // }
+        
+        // [HttpDelete]
+        // public IActionResult DeleteStudent([FromQuery] int id)
+        // {
+        //     if (id >= students.Count)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     students.RemoveAt(id);
+        //     return Ok("Usunieto studenta o id " + id);
+        // }
     }
 }
